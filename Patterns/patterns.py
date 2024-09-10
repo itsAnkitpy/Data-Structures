fb83d3a7ebfaacd994ddbad40895f1380407d513
itsAnkitpy <thegoat3585@gmail.com>
2024-09-10 12:52:52 +0530

    Added some more * patterns

--- Patterns/patterns.py
+++ Patterns/patterns.py
@@ -104,3 +104,238 @@
 #     for j in range(i):
 #         print("*", end="")
 #     print()
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Pattern 7
+
+# Reverse Hill Pattern
+
+#  *********
+#   *******
+#    *****
+#     ***
+#      *
+
+# n = 5
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" ", end="")
+#     for j in range(i,n):
+#         print("*", end="")
+#     for j in range(i,n-1):
+#         print("*", end="")
+#     print()
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Pattern 8
+
+# Diamond Pattern - Combination of uphill and reverse hill pattern
+
+#      *
+#     ***
+#    *****
+#   *******
+#  *********
+#  *********
+#   *******
+#    *****
+#     ***
+#      *
+
+# n = 5
+# for i in range(n):
+#     for j in range(i,n):
+#         print(" ", end="")
+#     for j in range(i+1):
+#         print("*", end="")
+#     for j in range(i):
+#         print("*", end="")
+#     print()
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" ", end="")
+#     for j in range(i,n):
+#         print("*", end="")
+#     for j in range(i,n-1):
+#         print("*", end="")
+#     print()
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Pattern 9
+
+# Sandglass Pattern - Reversal of Diamond Pattern
+
+#  *********
+#   *******
+#    *****
+#     ***
+#      *
+#      *
+#     ***
+#    *****
+#   *******
+#  *********
+
+# n = 5
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" ", end="")
+#     for j in range(i,n):
+#         print("*", end="")
+#     for j in range(i,n-1):
+#         print("*", end="")
+#     print()
+
+# for i in range(n):
+#     for j in range(i,n):
+#         print(" ", end="")
+#     for j in range(i+1):
+#         print("*", end="")
+#     for j in range(i):
+#         print("*", end="")
+#     print()
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Pattern 10
+
+# Double Hill Pattern
+
+#      *        *
+#     ***      ***
+#    *****    *****
+#   *******  *******
+#  ******************
+
+# n = 5 
+# for i in range(n):
+#     for j in range(i,n):
+#         print(" ", end="")
+#     for j in range(i+1):
+#         print("*", end="")
+#     for j in range(i):
+#         print("*", end="")
+#     for j in range(i,n-1):
+#         print(" ", end="")
+#     for j in range(i,n-1):
+#         print(" ", end="")
+#     for j in range(i+1):
+#         print("*", end="")
+#     for j in range(i):
+#         print("*", end="")
+#     print()
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Pattern 11
+
+# Left Pascal Triangle
+
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# * 
+
+# n = 5
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print("* ", end="")
+#     print()
+
+# for i in range(n):
+#     for j in range(i,n-1):
+#         print("* ", end="")
+#     print()
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Pattern 12
+
+# Right Pascal Triangle
+
+#         * 
+#       * * 
+#     * * * 
+#   * * * * 
+# * * * * * 
+#   * * * * 
+#     * * * 
+#       * * 
+#         * 
+
+# n = 5
+
+# for i in range(n):
+#     for j in range(i,n-1):
+#         print("  ", end="")
+#     for j in range(i+1):
+#         print("* ", end="")
+#     print()
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print("  ", end="")
+#     for j in range(i,n-1):
+#         print("* ", end="")
+#     print()
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Pattern 13
+
+# Butterfly Pattern
+
+# *                 * 
+# * *             * * 
+# * * *         * * * 
+# * * * *     * * * * 
+# * * * * * * * * * * 
+# * * * * * * * * * * 
+# * * * *     * * * * 
+# * * *         * * * 
+# * *             * * 
+# *                 * 
+
+# n = 5
+
+# for i in range(n):
+#     for j in range(i+1):
+#         print("* ", end="")
+#     for j in range(i,n-1):
+#         print("  ", end="")
+#     for j in range(i, n-1):
+#         print("  ",end= "")
+        
+#     for j in range(i+1):
+#         print("* ", end="")
+#     print()
+
+# for i in range(n):
+#     for j in range(i,n):
+#         print("* ",end= "")
+#     for j in range(i):
+#         print("  ", end="")
+#     for j in range(i):
+#         print("  ",end= "")
+        
+#     for j in range(i,n):
+#         print("* ", end="")
+#     print()
